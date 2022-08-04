3aee3fbe326d1cc667c4b30baeaf2d55a4f7b84e
[LuisVolt] <[agluisv@gmail.com]>
2022-08-04 01:31:28 -0500

    [creates  html, ask for  and stores input]

---
+++ script.js
@@ -0,0 +1,6 @@
+var inputNumber = parseInt(prompt("Enter a number"));
+for (let i = 1; i <= inputNumber;i++){
+    if(inputNumber%5 === "0" ){console.log(FizzBuzz);}
+    else {console.log(inputNumber);}
+    
+}
