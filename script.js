75a45c3096078b34854837630e0d04cdee729f35
[LuisVolt] <[agluisv@gmail.com]>
2022-08-04 01:40:38 -0500

    [prints counter until input is reached]

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+//  Need a prompt that asks for a number   ans stores in "inputNumber" as an integer
 var inputNumber = parseInt(prompt("Enter a number"));
-for (let i = 1; i <= inputNumber;i++){
-    if(inputNumber%5 === "0" ){console.log(FizzBuzz);}
-    else {console.log(inputNumber);}
-    
+
+// Need  for loop that displays a counter from 1 to the "inputNumber"
+for (let i = 1; i <= inputNumber; i++){
+    console.log(i);
 }
+
