e2a2979a64560bb83ce2763e3fdca90b10e54b81
[LuisVolt] <[agluisv@gmail.com]>
2022-08-05 23:31:30 -0500

    creates conditios for printing

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 //  Need a prompt that asks for a number   ans stores in "inputNumber" as an integer
-var inputNumber = parseInt(prompt("Enter a number"));
+let inputNumber = parseInt(prompt("Enter a number"));
 
 // Need  for loop that displays a counter from 1 to the "inputNumber"
 for (let i = 1; i <= inputNumber; i++){
-    console.log(i);
+    if(i%5 == 0 && i%3 == 0) {console.log('FizzBuzz \r');}
+    else if (i%5 == 0) {console.log('Buzz \r');}      
+    else if (i%3 == 0) {console.log('Fizz \r');} 
+    else{console.log(i);}
 }
 
